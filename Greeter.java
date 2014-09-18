f6998fd40c454e45ad9b6d7a0abb31d64c072893
tyleri <tyler.ishikawa@outlook.com>
2014-09-18 14:27:39 -0400

    Notes for 9/16 and 9/17

---
+++ Greeter.java
@@ -0,0 +1,14 @@
+public class Greeter {
+
+	// Instance Variables
+	private String message;
+
+	// Methods
+	public void setMessage( String m) {
+		message = m;
+	}
+
+	public void greet() {
+		System.out.println( message );
+	}
+}
