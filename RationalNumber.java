a3ea939a5c9124aed24cd3b603cf891cef56cb83
tyleri <tyler.ishikawa@outlook.com>
2014-10-07 14:44:33 -0400

    Added RationalNumber class

--- RationalNumber.java
+++ RationalNumber.java
@@ -23,25 +23,53 @@ public class RationalNumber {
 
 	// Methods
 
-	public int getNumer() {
-		return numer;
-	}
-
-	public int getDenom() {
-		return denom;
-	}
-
 	public void display() {
 		System.out.println( numer + "/" + denom );
 	}
 
-	public float floatValue() {
-		return (float) numer / denom;
+	public double floatValue() {
+		return (double) numer / denom;
 	}
 
 	public void multiply( RationalNumber rn ) {
-		numer *= rn.getNumer();
-		denom *= rn.getDenom();
+		numer *= rn.numer;
+		denom *= rn.denom;
+	}
+
+	public void add( RationalNumber rn ) {
+		int n1 = numer;
+		int d1 = denom;
+		int n2 = rn.numer;
+		int d2 = rn.denom;
+		denom = d1 * d2;
+		numer = n1 * d2 + n2 * d1;
+	}
+
+	public void subtract( RationalNumber rn ) {
+		int n1 = numer, d1 = denom, n2 = rn.numer, d2 = rn.denom;
+		denom = d1 * d2;
+		numer = n1 * d2 - n2 * d1;
+	}
+
+	public int gcd() {
+		int a = denom, b = numer;
+		if (a % b == 0)
+			return b;
+		else {
+			while (a % b != 0) {
+				int temp = a % b;
+				a = b;
+				b = temp;
+			}
+			return b;
+		}
+
+	}
+
+	public void reduce() {
+		int gcd = this.gcd();
+		numer /= gcd;
+		denom /= gcd;
 	}
 
 	// Main method
@@ -50,18 +78,43 @@ public class RationalNumber {
 
 		RationalNumber a = new RationalNumber();
 		RationalNumber b = new RationalNumber(5, 2);
+		System.out.printf("a = %f = ", a.floatValue());
 		a.display();
+		System.out.printf("b = %f = ", b.floatValue());
 		b.display();
-		System.out.printf("The float value of a is %f\n", a.floatValue());
-		System.out.printf("The float value of b is %f\n", b.floatValue());
 
+		System.out.println();
 		a = new RationalNumber(2, 7);
+		System.out.printf("a = %f = ", a.floatValue());
 		a.display();
 
+		System.out.println();
+		System.out.println("Multiplying b by a");
 		b.multiply(a);
+		System.out.printf("b = %f = ", b.floatValue());
 		b.display();
-		System.out.printf("The float value of b is now %f\n", b.floatValue());
 
+		System.out.println();
+		System.out.println("Adding a to b");
+		b.add(a);
+		System.out.printf("b = %f = ", b.floatValue());
+		b.display();
+
+		System.out.println();
+		System.out.println("Subtracting b by a");
+		b.subtract(a);
+		System.out.printf("b = %f = ", b.floatValue());
+		b.display();
+
+		System.out.println();
+		System.out.printf("The GCD of b is %d\n", b.gcd());
+
+		System.out.println();
+		System.out.println("Reducing b");
+		b.reduce();
+		System.out.printf("b = %f = ", b.floatValue());
+		b.display();
+		System.out.println();
 	}
 
 }
