489ba9fbdafada60bd38f322c546ea7eff4141ac
tyleri <tyler.ishikawa@outlook.com>
2014-10-21 17:41:48 -0400

    Added battle method to Room and removed methods from Map

--- RPG/Map.java
+++ RPG/Map.java
@@ -1,23 +1,25 @@
 public class Map {
 
-	private Room currRoom;
+	// private Room currRoom;
 	
 	public Map() {
 		currRoom = new Room(1);
 	}
 
+	/*
 	// moves the character into the next room
 	public void nextRoom () {
 		int nextLoc = currRoom.getLocation() + 1;
 		currRoom = new Room(nextLoc);
 	}
+	*/
 
 	// return a picture of the map, X marks your location
-	public String toString() {
+	public void display(int location) {
 		String map = "";
 		for (int i = 10; i > 0; i--) {
 			map += " (";
-			if (i == currRoom.getLocation())
+			if (i == location)
 				map += "X";
 			else
 				map += i + "";
--- RPG/Room.java
+++ RPG/Room.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class Room {
 
 	private int type; // 1 = fighting, 2 = shop, 3 = nothing
@@ -11,6 +14,45 @@ public class Room {
 	// Returns the number of the room
 	public int getLocation() {
 		return location;
+	}
+
+	public boolean battle(Character player, Character enemy) {
+		Scanner scan = new Scanner(System.in);
+		Random rand = new Random();
+		String pChoice = "";
+		int eChoice = -1;
+
+		while (player.getHealth() > 0 && enemy.getHealth() > 0) {
+
+			// Player's turn
+			while (pChoice == "") {
+				System.out.println("Pick an action:");
+				System.out.println("(A) Attack");
+				System.out.println("(B) Do Nothing");
+
+				pChoice = scan.nextLine();
+				if (pChoice == "A")
+					player.attack(enemy);
+				else if (pChoice == "B");
+				else
+					pChoice = "";
+			}
+
+			// Enemy's turn
+			eChoice = rand.nextInt(2);
+			if (eChoice == 0)
+				enemy.attack(player);
+		}
+
+		// After the battle
+		if (enemy.getHealth() == 0) {
+			System.out.println("You win!");
+			return true;
+		}
+		else {
+			System.out.println("You lose!");
+			return false;
+		}
 	}
 
 }
