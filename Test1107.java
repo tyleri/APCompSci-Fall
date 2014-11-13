8c22938ab13cca9803b32dc3260ae628eb1cb9cb
tyleri <tyler.ishikawa@outlook.com>
2014-11-13 14:39:12 -0500

    Notes and java file for 11/7

---
+++ Test1107.java
@@ -0,0 +1,15 @@
+public class Test1107 {
+
+	public static void main(String[] args) {
+		char c = '%';
+		byte b = 65;
+		short s = 94;
+		int i = 506;
+		long l = 3084;
+		float f = (float) .2;
+		double d = 1947.24;
+
+		System.out.println( (char) ('A' + 'C'));
+	}
+
+}
